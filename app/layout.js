bea373402d8472460f608c4d6fd6d15c6c876764
MURTAZA PATEL <murtazapatel89100@gmail.com>
2026-01-12 11:47:33 +0530

    fix: root size and colors

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased mx-auto max-w-7xl`}
+        className={`${inter.className} antialiased w-full container mx-auto max-w-7xl`}
       >
         {children}
       </body>
