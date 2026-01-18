ece8008e648a9c0eb834991a35b92e2a62a055f1
Pushkar-Ovhal <pushkarovhal2@gmail.com>
2026-01-18 13:06:37 +0530

    Teams section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Nav from './components/sections/Nav'
-import HeroSection from './components/sections/Hero'
-import MissionSection from './components/sections/Mission'
-import EventsSections from './components/sections/Events'
-import Team from './components/sections/Team'
-import WhyOurClub from './components/sections/WhyOurClub'
-import FAQs from './components/sections/FAQs'
-import FooterSection from './components/sections/Footer'
+import Nav from '../components/sections/Nav'
+import HeroSection from '../components/sections/Hero'
+import MissionSection from '../components/sections/Mission'
+import EventsSections from '../components/sections/Events'
+import Team from '../components/sections/Team'
+import WhyOurClub from '../components/sections/WhyOurClub'
+import FAQs from '../components/sections/FAQs'
+import FooterSection from '../components/sections/Footer'
 
 function LandingPage() {
   return (
